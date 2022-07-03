42c3cd854dd6ff6eec7fc81172905906d58ddcb2
AmineHazi <aminehazi03@gmail.com>
2022-07-03 23:27:58 +0100

    commit message

--- src/components/pages/Thl.js
+++ src/components/pages/Thl.js
@@ -1,6 +1,6 @@
 function Thl() {
   return (
-    <iframe src="https://drive.google.com/file/d/1Bty4DKdOusCBsulIbC1f15up02Xev4VR/preview"  width="100%" height="100%" allow="autoplay" title="report"></iframe>
+    <iframe src="https://drive.google.com/file/d/1bYwRyqXxFMz8ufBIDf4RZ-VwdT45lWBU/preview"  width="100%" height="100%" allow="autoplay" title="report"></iframe>
   )
 }
 
